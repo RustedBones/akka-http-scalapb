3edfe2ec4858e4e6bc7aad411a888980a77c969a
Scala Steward <me@scala-steward.org>
2021-03-05 22:42:34 +0100

    Update scalapb-json4s to 0.10.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val akkaHttp              = "10.2.0"
     val scalaCollectionCompat = "2.2.0"
     val scalaPB               = "0.10.8"
-    val scalaPBJson4s         = "0.10.1"
+    val scalaPBJson4s         = "0.10.3"
     val scalaTest             = "3.2.2"
   }
 
