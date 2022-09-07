986d230b068f0a2e2f22de8b5be049a3ae069a60
Scala Steward <scala_steward@virtuslab.com>
2022-09-07 19:51:26 +0000

    Update akka-stream, akka-testkit to 2.6.20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val Akka                  = "2.6.19"
+    val Akka                  = "2.6.20"
     val AkkaHttp              = "10.2.9"
     val ScalaCollectionCompat = "2.7.0"
     val ScalaPB               = "0.11.10"
